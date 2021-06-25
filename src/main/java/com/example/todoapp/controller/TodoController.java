8d2564a8a2469c614c61da3445e67189e7f94ff7
Ricardo Taipe <rdavid28@hotmail.es>
2021-06-24 21:42:59 -0500

    Add controller

---
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -0,0 +1,28 @@
+package com.example.todoapp.controller;
+
+import com.example.todoapp.domain.Todo;
+import com.example.todoapp.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/todos")
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<List<Todo>> getTodos() {
+        Optional<List<Todo>> todos = todoService.getTodos();
+        return todos.isPresent()
+                ? ResponseEntity.ok(todos.get())
+                : ResponseEntity.notFound().build();
+    }
+}
